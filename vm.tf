68956970b3c03149cf76ecad7296c2a10c63af42
sbriggs80 <simon.briggs@zthousand.com>
2025-04-09 15:20:17 +0100

    Update vm.tf 2

--- vm.tf
+++ vm.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec22" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0d5bb3742db8fc264"
   instance_type = "t3.micro"
   tags = {
     Name = "${terraform.workspace}-nwg-simon-w2-ec2"
