a3a4421972f8a8c4ba5b6eabef61e390b1d29d3c
sbriggs80 <simon.briggs@zthousand.com>
2025-04-09 14:48:03 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "buck1" {
-  bucket = "${terraform.workspace}-nwg-simon-github-bucket-1"
+  bucket = ${terraform.workspace}"-nwg-simon-github-bucket-1"
 }
 resource "aws_s3_bucket" "buck2" {
-  bucket = "${terraform.workspace}-nwg-simon-github-bucket-2"
+  bucket = ${terraform.workspace}"-nwg-simon-github-bucket-2"
 }
